33836e27051e87b4a079a3a1efe3c3ca1c3bbf83
muhammad <=muhammad992000a@gmail.com>
2024-05-23 23:08:33 +0100

    finis de header

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,19 +1,16 @@
-
-import { Navbar } from "./composants/navar";
-import "./globals.css";
-
-
+import BasicExample from '../components/navar';
+import styles from './globals.scss';
+import 'bootstrap/dist/css/bootstrap.css';
 
 export const metadata = {
-  title: "Vigitudiant",
-  description: "",
+  title: 'Vigitudiant',
+  description: '',
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <Navbar></Navbar>
-      <body >{children}</body>
+      <body className={styles.body}>{children}</body>
       <footer>footer</footer>
     </html>
   );
--- src/app/page.js
+++ src/app/page.js
@@ -1,17 +1,12 @@
-"use client"
-import styles from "./page.module.css";
-import Link from "next/link";
+'use client';
+import styles from './page.module.scss';
+import Link from 'next/link';
+import { Banner } from '../components/banner';
 
 export default function Home() {
   return (
-
     <main className={styles.main}>
-      <Link href="about" >about</Link>
-  
-
-
-
-      
+      <Banner></Banner>
     </main>
   );
 }
